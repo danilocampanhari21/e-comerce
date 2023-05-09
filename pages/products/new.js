f95d3869c6c037e834944b73a446a912e8cf95bb
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-05-09 18:30:53 +0200

    building api products

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,20 +1,46 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import { useState } from "react";
 
 export default function NewProduct() {
-    const [title, setTile] = useState('');
+    const [title, setTitle] = useState('');
     const [description, setDescription] = ('');
     const [price,setPrice] = useState('');
+    async function createProduct(){
+        const data = {title, description,price};
+        await axios.post('/api/products', data);
+    }
     return (
         <Layout>
+            <form onSubmit={createProduct}>
             <h1>New Product</h1>
             <label>Product name</label>
-            <input type="text" placeholder="product name"></input>
+            <input type="text" 
+            placeholder="product name" 
+            value={title} 
+            onChange={ev => setTitle (ev.target.value)}></input>
+
             <label>Description</label>
-            <textarea placeholder="description"></textarea>
+            <textarea 
+            placeholder="description" 
+            value={description}
+            onChange={ev => setDescription(ev.target.value)}
+            ></textarea>
+
             <label>Price (in USD)</label>
-            <input type="number" placeholder="price"></input>
-            <button className="btn-primary">Save</button>
+            <input type="number" 
+            placeholder="price" 
+            value={price}
+            onChange={ev => setPrice(ev.target.value)}
+            ></input>
+
+            <button 
+                type="submit" 
+                className="btn-primary">Save
+            </button>
+
+            </form>
+            
         </Layout>
     )
 }
