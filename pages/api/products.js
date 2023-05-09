7c1134ca273341c6832878e6193ae6b117391752
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-05-09 22:02:55 +0200

    building mongoose

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,6 +1,13 @@
-export default function handle(req, res) {
+import { Product } from "@/models/Product";
+import mongoose from "mongoose";
+
+export default async function handle(req, res) {
     const {method} = req;
     if (method === 'POST') {
-        
+        const {title, description,price} = req.body;
+        const productDoc = await Product.create({
+            title,description,price,
+        })
+        res.json(productDoc);
     }
 }
