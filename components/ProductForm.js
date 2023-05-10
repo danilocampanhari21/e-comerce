12140ccec915e02f05675e4c706f3b50c0e2e789
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-05-10 16:54:22 +0200

    building continue

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 
 export default function ProductForm({
+    _id,
         title:existingTitle,
         description:existingdescription,
         price:existingPrice,
@@ -12,17 +13,24 @@ export default function ProductForm({
     const [price,setPrice] = useState(existingPrice || '');
     const [goToProducts, setGoToProducts] = useState(false);
     const router = useRouter();
-    async function createProduct(ev) {
+    async function saveProduct(ev) {
         ev.preventDefault();
-        const data = {title, description,price};
-        await axios.post('/api/products', data);
-        setGoToProducts(true);
+        if (_id) {
+            //update
+            await axios.put('/api/products', data);
+        } else {
+            //create
+            const data = {title, description,price};
+            await axios.post('/api/products', data);
+            setGoToProducts(true);
+        }
+        
     }
     if(goToProducts) {
         router.push('/products');
     }
     return (
-            <form onSubmit={createProduct}>
+            <form onSubmit={saveProduct}>
             <label>Product name</label>
             <input type="text" 
             placeholder="product name" 
